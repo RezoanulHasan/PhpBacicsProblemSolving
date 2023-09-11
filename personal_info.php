5ad4190edc45fd37d262362cd00646dffb22472a
RezoanulHasan <rezoanulhasan96@gmail.com>
2023-09-11 22:01:16 +0600

    add problem solving 1

---
+++ personal_info.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><!DOCTYPE html>
+
+    <title>Personal Information</title>
+</head>
+<body>
+
+<?php
+// Define variables
+$name = "Rezoanul Hasan";
+$age = 25;
+$country = "Bogar,Bangladesh";
+$introduction = "Hello! My name is Rezoanul Hasan.I am Web developer .";
+
+// Display personal information
+echo "<h1>Personal Information</h1>";
+echo "<p><strong>Name:</strong> $name</p>";
+echo "<p><strong>Age:</strong> $age</p>";
+echo "<p><strong>Country:</strong> $country</p>";
+echo "<p><strong>Introduction:</strong> $introduction</p>";
+?>
+
+</body>
+</html>
